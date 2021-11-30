66274dd414b58c6ec18ed197c0aa01fb1fbaa5f2
SakshiSonalkar <sonalkarsakshi@gmail.com>
2021-11-30 21:16:01 +0530

    Inheritance Example

---
+++ Day4/src/Shape.java
@@ -0,0 +1,112 @@
+
+class Rectangle{
+    public double length,breadth;
+    public double area(){
+        return length * breadth;
+    }
+}
+class Cuboid extends Rectangle{
+    public double height;
+    public double volume(){
+        return area() * height;
+    }
+}
+
+class Shape {
+    public static void main(String[] args){
+        Cuboid cuboid = new Cuboid();
+        cuboid.length=3;
+        cuboid.breadth=4;
+        cuboid.height=5;
+        System.out.println(cuboid.volume());
+        System.out.println(cuboid.area());
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
