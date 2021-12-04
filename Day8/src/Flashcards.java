270ac721e21491caf03d23b19311d2fca4ddf8aa
SakshiSonalkar <sonalkarsakshi@gmail.com>
2021-12-04 22:55:57 +0530

    BankAccount Program

---
+++ Day8/src/Flashcards.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+
+public class Flashcards {
+    public static void main(){
+        HashMap<String,Integer> flashcards = new HashMap<>();
+        flashcards.put("England took over Bombay",1957);
+        flashcards.put("Battle at Panipat",1761);
+
+    }
+}
