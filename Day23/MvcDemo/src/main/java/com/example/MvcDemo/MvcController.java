5beb39050d1254a08a81d03f9a8e80032cacfb7f
SakshiSonalkar <sonalkarsakshi@gmail.com>
2021-12-29 11:03:25 +0530

    MVC Demo -Day 23,Day28

--- Day23/MvcDemo/src/main/java/com/example/MvcDemo/MvcController.java
+++ Day23/MvcDemo/src/main/java/com/example/MvcDemo/MvcController.java
@@ -3,9 +3,11 @@ package com.example.MvcDemo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
 
 @Controller
 public class MvcController {
@@ -26,7 +28,7 @@ public class MvcController {
         return "add";
     }
 
-    @RequestMapping("/result")
+    @RequestMapping("/result")    //---->GetMapping w/o any attributes
     public String result(Model model, HttpServletRequest req){
         int num1 = Integer.parseInt(req.getParameter("num1"));
         int num2 = Integer.parseInt(req.getParameter("num2"));
@@ -36,8 +38,48 @@ public class MvcController {
         return "result";
     }
 
-    @RequestMapping("/login")
-    public String login(){
-        return "login";
+    @RequestMapping("/register")
+    public String register(){
+        return "register";
     }
-}
+
+    @PostMapping("/details")
+    public String details(Model model,HttpServletRequest req){
+        String username= req.getParameter("username");
+        String password = req.getParameter("password");
+        String email = req.getParameter("email");
+        String address= req.getParameter("address");
+
+//        Enumeration<String> enumeration = req.getHeaderNames();
+//        while(enumeration.hasMoreElements()){
+//            System.out.println(enumeration.nextElement());
+//        }
+//        while(enumeration.hasMoreElements()){
+//            String headerName=(String) enumeration.nextElement();
+//            String headerValue=req.getHeader(headerName);
+//            System.out.println("<b>" + headerName + "</b>");
+//            System.out.println((headerValue + "<br>"));
+
+        model.addAttribute("username",username);
+        model.addAttribute("email",email);
+        model.addAttribute("address",address);
+
+        return "details";
+        }
+
+    }
+
+
+
+//Restful API
+//  Url,Less
+//--->GET-READ
+
+  //Http header,more
+//--->POST-CREATE (for sensitive data)
+//--->PUT/PATCH-UPDATE
+//--->DELETE- DELETE  (CRUD)
+//RequestMapping knows how to get data and accessing/mapping web pages etc etc
+
+//details put
+//details get,post delete are different paths
