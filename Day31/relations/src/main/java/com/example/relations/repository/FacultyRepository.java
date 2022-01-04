5627ec258f0c242a1d7975f92b3844f12c02aba6
SakshiSonalkar <59169461+SakshiSonalkar@users.noreply.github.com>
2022-01-05 00:02:43 +0530

    Day 31-JPA relations

--- Day31/relations/src/main/java/com/example/relations/repository/FacultyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.relations.repository;
-
-import com.example.relations.entity.Faculty;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FacultyRepository extends JpaRepository<Faculty,Long> {
-}
