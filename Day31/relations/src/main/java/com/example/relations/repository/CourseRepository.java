e327b976e909f99e9936afdd896a8b6b6d27604b
SakshiSonalkar <59169461+SakshiSonalkar@users.noreply.github.com>
2022-01-04 09:38:28 +0530

    Delete Day31 directory

--- Day31/relations/src/main/java/com/example/relations/repository/CourseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.relations.repository;
-
-import com.example.relations.entity.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course,Long>{
-
-}
