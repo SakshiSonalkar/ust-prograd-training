645c0618dd39bbf96e081d507be65bfa66d0685c
SakshiSonalkar <59169461+SakshiSonalkar@users.noreply.github.com>
2022-02-01 15:15:12 +0530

    HackerRank Practice

--- BookMovieTickets/src/main/java/com/project/BookMovieTickets/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.BookMovieTickets.repository;
-
-import com.project.BookMovieTickets.entity.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Users,String> {
-}
